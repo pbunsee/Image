fde60113bee766b2f746944ea483b67b7a742aab
Pranesha Bunsee <pbunsee@gmail.com>
2015-08-03 18:52:44 -0400

    Added "new" method to  products_controller Added new and show views for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
