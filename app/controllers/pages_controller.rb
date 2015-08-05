a7c63c3dcf6e371d8eaae6a097e91d56133012d4
KATIE shum <katieshum@KATIEs-MacBook-Pro.local>
2015-08-04 23:57:17 -0400

    updated contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,24 @@ class PagesController < ApplicationController
   end
 
   def contact
+	name = params["name"]
+	message = params["message"]
+	
+	mandrill = Mandrill::API.new ENV['MANDRILL_APIKEY']
+	
+	message_to_mandrill = {
+		:subject=> "Message from project site",
+		:from_name=> @name,
+		:text=> message,
+		:to=> [{
+			:email=> "shum.katie@gmail.com",
+			:name=> "IMAGE-NY"
+		}],
+		:html=> "<html>test html string</html>",
+		:from_email=> "shum.katie@gmail.com"
+	}
+	sending = mandrill.messages.send message_to_mandrill
+	puts sending
+
   end
 end
