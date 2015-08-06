25c249f05fe6f0bff02b8028af3f7022992123db
Pranesha Bunsee <pbunsee@gmail.com>
2015-08-06 00:11:07 -0400

    Converted each line item object to JSON string Persisted each JSON string (line item) as a cookie Push onto array of line items (array of objects) each time the 'Add to Cart' button is clicked. Splice the line item object that is being removed from cart out of the array of objects/line items.

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,14 +1,62 @@
+function setCookie(obj, cookieName) {
+  console.dir(obj);
+  console.log("cookieName is: " + cookieName);
+  var tempStr = JSON.stringify(obj);
+  console.log("tempStr is: " + tempStr);
+  Cookies.set(cookieName, tempStr);
+}
 
-function addItem(){
+function getCookie(cookieName) {
+  console.log("cookieName is: " + cookieName);
+  var tempStr = Cookies.get(cookieName);
+  var objName = JSON.parse(tempStr);
+  //console.dir(objName);
+  return objName;
+}
+
+function removeCookie(){
+}
+
+function addItem(line_item){
   console.log("adding item to cart");
+  setCookie(line_item, 'lineItemCookie');
+  addToCart(line_item);
+    console.dir(arrCart);
+}
+
+function getItem(){
+  console.log("getting item from cart");
+  var line_item = getCookie('lineItemCookie');
+  console.dir(line_item);
 }
 
 function removeItem(){
   console.log("removing item from cart");
+  //removeCookie('lineItemCookie', line_item);
+  removeFromCart(line_item);
 }
 
-function createCart(){
-  console.log("Creating cart");
+function getCart() {
+  console.log("Getting a cart");
+  // instantiate the cart array if it doesn't already exist
+  if (typeof arrCart == "undefined") {
+    console.log("New cart");
+    var tempCart = [];
+  }
+  return tempCart;
+    console.dir(tempCart);
+}
+
+function addToCart(objLineItem){
+  console.log("Adding to cart");
+  arrCart.push(objLineItem);
+    console.dir(arrCart);
+}
+
+function removeFromCart(objLineItem){
+  console.log("Removing line item from cart");
+  var atIndex = arrCart.indexOf(objLineItem);
+  arrCart.splice(atIndex, 1);
 }
 
 function emptyCart(){
@@ -27,15 +75,24 @@ function decrementQty(){
   console.log("Decrementing product inventory");
 }
 
+// set the cart as global variable
+console.log("before document ready do some prep");
+var arrCart = getCart();
+console.dir(arrCart);
+
 $(document).on('ready page:load',function(){
 
   console.log('ready!');
 
-  //listen for onclick event on Add Cart
+  //listen for onclick event on 'Add to Cart' link
   $('.add-btn').click(function(){
-    console.log( "Handler for Add Cart .click() called." );
-    line_item = $(this).data('line-item');
+    console.log( "Handler for Add product into cart .click() called." );
+    var line_item = $(this).data('line-item');
     console.dir(line_item);
+    console.dir(arrCart);
+    addItem(line_item);
+    console.dir(arrCart);
+    getItem();
   });
 });
 
