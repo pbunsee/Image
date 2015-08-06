d07d104b2df1831f291aaed4d9147ca7f09ba846
Sabiha Ozturk <sabihay@hotmail.com>
2015-08-06 17:40:29 -0400

    Fixed style and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
      current_order = Order.find(session[:order_id])
    end
  end
+ def current_user
+   if session[:user_id]
+     current_user = User.find(session[:user_id])
+   end
+ end
 end
