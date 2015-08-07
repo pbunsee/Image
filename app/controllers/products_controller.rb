70254bdf2d5361b524e8579e083a5d7f7e7fd930
Pranesha Bunsee <pbunsee@gmail.com>
2015-08-06 23:50:50 -0400

    Add gon watch without any interval specified for once-off refresh of product variable when attempting to add a product to the cart. Tests were inconclusive - need to re-visit this branch.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    gon.product = @product
+    gon.watch.product = @product
   end
 
   def edit
