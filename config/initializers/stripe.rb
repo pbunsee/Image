c507470bb2ebe843b3c7298444b58ab99b96a1f6
Katie Shum <shum.katie@gmail.com>
2015-08-08 12:59:56 -0400

    got stripe testing mode incorporated

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['pk_test_DLeacr8Dmx6ik7owXk6S79xa'],
-  secret_key:      ENV['sk_test_egDTSGf9uBZu4K4XfM0zWd7i'],
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
