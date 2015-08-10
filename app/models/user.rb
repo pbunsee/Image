6d9991139c3cd2c4672b222673862c579b14d276
Vinson Lee <vinson.lee.ny@gmail.com>
2015-08-10 13:49:59 -0400

    Heroku commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", small: "150x150>"}, 
-                    :url => "/assets/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
+#  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", small: "150x150>"}, 
+#                     :url => "/assets/:id/:style/:basename.:extension",
+#                     :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+#   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
